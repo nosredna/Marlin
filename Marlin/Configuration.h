112a84fe13b74c9635d9773548b28ed946232d1e
Anderson Nascimento <anderson@spensatech.com>
2019-08-21 02:56:28 -0400

    fix default steps per unit for 256 step divisions
    vs 16 step default

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -728,7 +728,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 1280, 1280, 400, 6700, 6700 }
 
 /**
  * Default Max Feed Rate (mm/s)
